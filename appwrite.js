56bb0217da960c72083e490f65f5a1260263f1db
ilsxzhub <ethanoka94@gmail.com>
2025-11-16 09:47:55 -0500

    Update appwrite.js

--- appwrite.js
+++ appwrite.js
@@ -1,13 +1,13 @@
 // appwrite.js
-import { Client, Account, Databases, Storage } from 'appwrite';
+import { Client, Account, Databases, Storage } from "https://esm.run/appwrite";
 
-const client = new Client();
-
-client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // "https://tor.cloud.appwrite.io/v1"
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // "69193dc300018a78bd84"
+const client = new Client()
+  .setEndpoint("https://tor.cloud.appwrite.io/v1") // Your endpoint
+  .setProject("69193dc300018a78bd84");             // Your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
+export { client };
+
 
